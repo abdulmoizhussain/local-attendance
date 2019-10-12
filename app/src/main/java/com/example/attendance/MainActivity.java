7dd37cae5345430fc62497f3d06d093eb29a045d
unknown <moizhus123@gmail.com>
2019-10-13 00:00:17 +0500

    set up project

---
+++ app/src/main/java/com/example/attendance/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.attendance;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		BottomNavigationView navView = findViewById(R.id.nav_view);
+		// Passing each menu ID as a set of Ids because each
+		// menu should be considered as top level destinations.
+		AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+				R.id.navigation_status, R.id.navigation_logs)
+				.build();
+		NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+		NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+		NavigationUI.setupWithNavController(navView, navController);
+		
+		// So that bottom of parent-fragment can end at the top of bottom-tab-navigation-bar.
+		DisplayMetrics navMetrics = navView.getResources().getDisplayMetrics();
+		View parentFragment = findViewById(R.id.nav_host_fragment);
+		DisplayMetrics fragmentMetrics = parentFragment.getResources().getDisplayMetrics();
+		parentFragment.getLayoutParams().width = fragmentMetrics.widthPixels - navMetrics.widthPixels;
+		parentFragment.getLayoutParams().height = fragmentMetrics.heightPixels - navMetrics.heightPixels;
+	}
+}
